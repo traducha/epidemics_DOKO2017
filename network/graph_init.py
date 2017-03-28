0b05a174ec6f98854261e21500275504ed74547f
tomaszraducha <raducha.tomasz@gmail.com>
2017-03-28 16:10:30 +0200

    wielkość wierzchołków

--- network/graph_init.py
+++ network/graph_init.py
@@ -9,10 +9,12 @@ m = 2
 
 # inicjalizacja i rysowanie sieci losowej
 g = ig.Graph.Erdos_Renyi(n=N, m=M)
+g.vs()['size'] = [i + 10 for i in g.degree()]
 layout = ig.Graph.layout_kamada_kawai(g)
 ig.plot(g, layout=layout)
 
 # inicjalizacja i rysowanie sieci bezskalowej
 g = ig.Graph.Barabasi(n=N, m=m)
+g.vs()['size'] = [i + 10 for i in g.degree()]
 layout = ig.Graph.layout_kamada_kawai(g)
 ig.plot(g, layout=layout)
