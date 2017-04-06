31c5ef8b1df3630075a52b45bb9c9621102300b9
tomaszraducha <raducha.tomasz@gmail.com>
2017-04-06 19:09:45 +0200

    generate

--- workshop/odp/generate.py
+++ workshop/odp/generate.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from random import randint
 import uuid
 
-print str(uuid.uuid4())[:8]
+files_num = 200
 
-for i in xrange(30):
+for i in xrange(files_num):
     with open(str(uuid.uuid4())[:8] + '.py', 'w+') as _file:
-        pass
+        for j in xrange(randint(10, 100)):
+            if randint(1, 4) == 1:
+                _file.write('\n')
+            else:
+                _file.write(str(uuid.uuid4()).replace('-', '')[:randint(1, 32)] + '\n')
